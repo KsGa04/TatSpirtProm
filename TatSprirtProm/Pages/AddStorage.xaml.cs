0e667c658666b59385ef055b662c38c3b4cf8d49
Ксения Гаранчева <GaranchevaKS@stud.kai.ru>
2024-05-02 18:21:12 +0300

    Добавьте файлы проекта.

---
+++ TatSprirtProm/Pages/AddStorage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TatSprirtProm.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddStorage.xaml
+    /// </summary>
+    public partial class AddStorage : Page
+    {
+        public AddStorage()
+        {
+            InitializeComponent();
+        }
+    }
+}
