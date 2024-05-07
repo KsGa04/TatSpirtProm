6f6607aea9cdfae5083afa0c5d808443b5bd85cd
Ксения Гаранчева <GaranchevaKS@stud.kai.ru>
2024-05-07 14:28:05 +0300

    Корректировки

--- TatSprirtProm/Pages/NewDelivery.xaml.cs
+++ TatSprirtProm/Pages/NewDelivery.xaml.cs
@@ -20,9 +20,32 @@ namespace TatSprirtProm.Pages
     /// </summary>
     public partial class NewDelivery : Page
     {
+        public TatSpirtPromEntities db = new TatSpirtPromEntities();
         public NewDelivery()
         {
             InitializeComponent();
+            txtPrice.IsEnabled = false;
+            foreach (var d in db.Product)
+            {
+                cbSupplier.Items.Add(d.name_product);
+            }
+            foreach (var d in db.Storage)
+            {
+                cbWarehouse.Items.Add(d.address);
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Delivery delivery = new Delivery();
+            delivery.data_delivery = (DateTime)dpDeliveryDate.SelectedDate;
+            delivery.comment = txtComment.Text;
+            delivery.total_price = Convert.ToDecimal(txtPrice.Text);
+            delivery.count = Convert.ToInt32(txtQuantity.Text);
+            delivery.id_storage = cbWarehouse.SelectedIndex;
+            delivery.id_product = cbSupplier.SelectedIndex;
+            db.Delivery.Add(delivery);
+            db.SaveChanges();
         }
     }
 }
--- TatSprirtProm/Pages/NewOrder.xaml.cs
+++ TatSprirtProm/Pages/NewOrder.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Class;
 
 namespace TatSprirtProm.Pages
 {
@@ -20,9 +21,27 @@ namespace TatSprirtProm.Pages
     /// </summary>
     public partial class NewOrder : Page
     {
+        public TatSpirtPromEntities db = new TatSpirtPromEntities();
         public NewOrder()
         {
             InitializeComponent();
+            txtPrice.IsEnabled = false;
+            foreach (var d in db.Product)
+            {
+                cbSupplier.Items.Add(d.name_product);
+            }
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Orders delivery = new Orders();
+            delivery.data_order = (DateTime)dpDeliveryDate.SelectedDate;
+            delivery.comment = txtComment.Text;
+            delivery.total_price = Convert.ToDecimal(txtPrice.Text);
+            delivery.count = Convert.ToInt32(txtQuantity.Text);
+            delivery.id_product = cbSupplier.SelectedIndex;
+            delivery.id_organization = Auth.UserID;
+            db.Orders.Add(delivery);
+            db.SaveChanges();
         }
     }
 }
